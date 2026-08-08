4f212d5ea67ac80c7d44de898c8a944af19067bf
mainak-github <rickbhattacherjee@gmail.com>
2026-08-08 17:29:25 +0530

    refactor: remove manual chunk splitting in vite config and update chunk size warning limit

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,24 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'react-vendor';
-            }
-            if (id.includes('antd') || id.includes('@ant-design')) {
-              return 'antd-vendor';
-            }
-            if (id.includes('font-awesome') || id.includes('line-awesome')) {
-              return 'icons-vendor';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 1600
   }
 })
